cd611823d351d05537caf3c0b868e40a7a3b02cc
Bob McNabb <robertmcnabb@gmail.com>
2017-03-27 11:04:09 +0200

    update to version 0.11.1: added shell scripts

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='pybob',
-    version='0.11',
+    version='0.11.1',
     description='Collection of geospatial and other tools I find useful.',
     url='http://github.com/iamdonovan/pybob',
     author='Bob McNabb',
@@ -13,4 +13,5 @@ setup(name='pybob',
         'shapely', 'opencv-python', 'pandas', 'geopandas',
         'scikit-image', 
     ],
+    scripts=['bin/dem_coregistration.py', 'bin/generate_panchromatic.py', 'bin/print_reverb_browse_urls.py', 'bin/print_reverb_granule_names.py'],
     zip_safe=False)
