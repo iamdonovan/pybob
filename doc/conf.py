71ccbc9d2b0f550d90c4b16d11584752671c1bdb
Bob McNabb <robertmcnabb@gmail.com>
2019-06-12 01:23:37 +0200

    update doc/conf.py to include required packages

--- doc/conf.py
+++ doc/conf.py
@@ -16,7 +16,9 @@ import os
 import sys
 sys.path.insert(0, os.path.abspath('..'))
 
-autodoc_mock_imports = ["osgeo"]
+autodoc_mock_imports = ['numpy', 'scipy', 'matplotlib', 'fiona',
+                        'shapely', 'opencv-python', 'pandas', 'geopandas',
+                        'scikit-image', 'gdal', 'h5py', 'pyproj', 'osgeo']
 
 def skip(app, what, name, obj, would_skip, options):
     if name == "__init__":
