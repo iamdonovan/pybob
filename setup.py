7e3821328a82b0bddbb8780b0524c4f09474c1dc
Bob McNabb <robertmcnabb@gmail.com>
2019-06-13 13:58:23 +0200

    update setup.py to include sphinx-argparse, remove non-existent scripts

--- setup.py
+++ setup.py
@@ -11,12 +11,11 @@ setup(name='pybob',
       install_requires=[
                         'numpy', 'scipy', 'matplotlib', 'fiona', 'pyvips',
                         'shapely', 'opencv-python', 'pandas', 'geopandas',
-                        'scikit-image', 'gdal', 'h5py', 'pyproj', 'descartes'
-                         ],
+                        'scikit-image', 'gdal', 'h5py', 'pyproj', 'descartes',
+                        'sphinx-argparse'],
       scripts=['bin/dem_coregistration.py', 'bin/generate_panchromatic.py',
-               'bin/print_reverb_browse_urls.py', 'bin/print_reverb_granule_names.py',
                'bin/find_aster_dem_pairs.py', 'bin/image_footprint.py',
                'bin/write_qgis_meta.py', 'bin/dem_difference.py',
                'bin/calculate_mmaster_dh_curves.py', 'bin/image_footprint_from_met.py',
-			   'bin/dem_coregistration_grid.py','bin/extract_ICESat.py'],
+               'bin/dem_coregistration_grid.py','bin/extract_ICESat.py'],
       zip_safe=False)
