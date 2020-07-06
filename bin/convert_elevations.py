5ed43df2efc4eac76aba995da36b45fe4ed9326c
Bob McNabb <robertmcnabb@gmail.com>
2020-07-06 11:32:32 +0100

    fix argparser issues

--- bin/convert_elevations.py
+++ bin/convert_elevations.py
@@ -7,10 +7,10 @@ def _argparser():
     parser = argparse.ArgumentParser(description="""Convert DEM from geoid to ellipsoid heights (or vice-versa)""")
     parser.add_argument('dem', type=str, help='DEM to convert')
     parser.add_argument('geoid', type=str, help='geoid file')
-    parser.add_argument('-g', 'to_geoid', action='store_true', type=bool, default=False,
+    parser.add_argument('-g', '--to_geoid', action='store_true', default=False,
                         help="Convert to geoid heights (i.e., subtract geoid model from dem) [False]")
-    parser.add_argument('-o', 'out_folder', type=str, default=None,
-                        help='Folder to write converted DEM to. [same as input]")
+    parser.add_argument('-o', '--out_folder', type=str, default=None,
+                        help='Folder to write converted DEM to. [same as input]')
     return parser
 
 
