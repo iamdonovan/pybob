97cb1a71cd1a8ae04aa1c6cbb9b9bf545c23fd64
Bob McNabb <robertmcnabb@gmail.com>
2021-06-22 15:06:38 +0100

    change import structure

--- pybob/__init__.py
+++ pybob/__init__.py
@@ -1,13 +1,13 @@
 """
 pybob is a python package of geospatial and other tools that I find useful.
 """
-import pybob.bob_tools
-import pybob.image_tools
-import pybob.continuity_tools
-import pybob.coreg_tools
-import pybob.ddem_tools
-import pybob.plot_tools
-import pybob.GeoImg
-import pybob.VelField
-import pybob.ICESat
-import pybob.landsat_tools
+from . import bob_tools
+from . import image_tools
+from . import continuity_tools
+from . import coreg_tools
+from . import ddem_tools
+from . import plot_tools
+from . import GeoImg
+from . import VelField
+from . import ICESat
+from . import landsat_tools
