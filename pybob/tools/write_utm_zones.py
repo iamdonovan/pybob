b9a9316e90ae5e1e9717b58decac5214f0cce585
Bob McNabb <robertmcnabb@gmail.com>
2021-06-22 13:00:08 +0100

    if entrypoint calls main, main should exist.

--- pybob/tools/write_utm_zones.py
+++ pybob/tools/write_utm_zones.py
@@ -1,8 +1,14 @@
 #!/usr/bin/env python
 import geopandas as gpd                                                                                                                                                                               
 
-singles = gpd.read_file('Footprints.shp')                                                                                                                                                             
 
-with open('zone_list.txt', 'w') as f: 
-    for i, row in singles.iterrows():
-        print('{} {}'.format(row.filename, row.best_zone), file=f)
+def main():
+    singles = gpd.read_file('Footprints.shp')
+
+    with open('zone_list.txt', 'w') as f:
+        for i, row in singles.iterrows():
+            print('{} {}'.format(row.filename, row.best_zone), file=f)
+
+
+if __name__ == "__main__":
+    main()
