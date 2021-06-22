05d94cb90db9cc34666cd9a904f6e5f37b78f5b1
Bob McNabb <robertmcnabb@gmail.com>
2021-06-22 12:58:20 +0100

    update to use entrypoints and move the license file to the main directory.

--- setup.py
+++ setup.py
@@ -1,23 +1,51 @@
-from setuptools import setup
+from pathlib import Path
+from setuptools import find_packages, setup
+
+readme = Path(__file__).parent / 'README.md'
 
 setup(name='pybob',
-      version='0.25.1',
+      version='0.26',
       description='Collection of geospatial and other tools I find useful.',
-      url='http://github.com/iamdonovan/pybob',
+      long_description=readme.read_text(),
+      long_description_content_type='text/markdown',
+      url='https://github.com/iamdonovan/pybob',
+      doc_url='https://pybob.readthedocs.io/',
       author='Bob McNabb',
       author_email='robertmcnabb@gmail.com',
+      maintainer='iamdonovan',
       license='MIT',
+      license_file='LICENSE',
+      classifiers=[
+          'Intended Audience :: Science/Research',
+          'License :: OSI Approved :: MIT License',
+          'Natural Language :: English',
+          'Programming Language :: Python :: 3',
+          'Programming Language :: Python :: 3.7',
+          'Programming Language :: Python :: 3.8',
+          'Programming Language :: Python :: 3.9',
+      ],
       packages=['pybob'],
+      python_requires='>=3.7',
       install_requires=[
                         'numpy', 'scipy', 'matplotlib', 'fiona', 'pyvips',
                         'shapely', 'opencv-python', 'pandas', 'geopandas',
-                        'scikit-image>=0.18', 'gdal', 'h5py', 'pyproj', 'llc', 'numba', 'descartes',
+                        'scikit-image>=0.18', 'gdal', 'h5py', 'pyproj', 'numba', 'descartes',
                         'sphinx-argparse'],
-      scripts=['bin/dem_coregistration.py', 'bin/generate_panchromatic.py',
-               'bin/find_aster_dem_pairs.py', 'bin/image_footprint.py',
-               'bin/write_qgis_meta.py', 'bin/dem_difference.py',
-               'bin/calculate_mmaster_dh_curves.py', 'bin/image_footprint_from_met.py',
-               'bin/dem_coregistration_grid.py','bin/extract_ICESat.py',
-               'bin/write_utm_zones.py', 'bin/register_landsat.py',
-               'bin/orthorectify_landsat.py', 'bin/convert_elevations.py'],
+      entrypoints={
+          'console_scripts': [
+              'calculate_mmaster_dh_curves = pybob.tools.calculate_mmaster_dh_curves:main',
+              'convert_elevations = pybob.tools.convert_elevations:main',
+              'dem_coregistration = pybob.tools.dem_coregistration:main',
+              'dem_coregistration_grid = pybob.tools.dem_coregistration_grid:main',
+              'dem_difference = pybob.tools.dem_difference:main',
+              'extract_ICESat = pybob.tools.extract_ICESat:main',
+              'find_aster_dem_pairs = pybob.tools.find_aster_dem_pairs:main',
+              'generate_panchromatic = pybob.tools.generate_panchronmatic:main',
+              'image_footprint = pybob.tools.image_footprint:main',
+              'image_footprint_from_met = pybob.tools.image_footprint_from_met:main',
+              'orthorectify_landsat = pybob.tools.orthorectify_landsat:main',
+              'register_landsat = pybob.tools.register_landsat:main',
+              'write_utm_zones = pybob.tools.write_utm_zones:main',
+          ]
+      },
       zip_safe=False)
